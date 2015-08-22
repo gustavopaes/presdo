f5ae2f5d6bfbcec9449d29c0557112edd2efeefa
Gustavo Paes <gustavo.paes@gmail.com>
2015-08-22 11:43:28 -0300

    Move all markdown functions to markdown file

--- markdown.go
+++ markdown.go
@@ -1,3 +1,107 @@
 package presdo
 
+import (
+  "strings"
+  "html/template"
+  "github.com/russross/blackfriday"
+  "io/ioutil"
+  "log"
+  "os"
+  "time"
+)
 
+type MarkdownStruct struct { }
+
+var markdown MarkdownStruct = MarkdownStruct{}
+
+func (md *MarkdownStruct) Parse(requestFilePath string, markdownPath string) template.HTML {
+    markdownContent, _ := ioutil.ReadFile(markdownPath)
+
+    var pageContent Page
+
+    // Get all header fields
+    extractHeader(markdownContent, &pageContent)
+
+    pageHtml := view.HTML(&pageContent)
+
+    save(requestFilePath, markdownPath, pageHtml)
+
+    return pageHtml
+}
+
+// configure markdown render options
+// See blackfriday markdown source for details
+func render(content []byte) template.HTML {
+  htmlFlags := 0
+  //htmlFlags |= blackfriday.HTML_SKIP_SCRIPT
+  htmlFlags |= blackfriday.HTML_USE_XHTML
+  htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+  htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+  htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+  renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+
+  extensions := 0
+  extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+  extensions |= blackfriday.EXTENSION_TABLES
+  extensions |= blackfriday.EXTENSION_FENCED_CODE
+  extensions |= blackfriday.EXTENSION_AUTOLINK
+  extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+  extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+
+  return template.HTML(blackfriday.Markdown([]byte(content), renderer, extensions))
+}
+
+func save(requestFilePath string, markdownPath string, parsedMarkdown template.HTML) {
+    cachedPath := paths.Cache(requestFilePath)
+
+    if err := ioutil.WriteFile(cachedPath, []byte(parsedMarkdown), 0644); err != nil {
+        log.Println("Error on create markdown cache file: ", err)
+    } else {
+        if markdownStat, err := os.Stat(markdownPath); err == nil {
+            os.Chtimes(cachedPath, markdownStat.ModTime(), markdownStat.ModTime())
+        }
+    }
+}
+
+func extractHeader(markdownContent []byte, page *Page) {
+    page.Params = make(map[string]string)
+
+    var lines = strings.Split(string(markdownContent), "\n")
+    var found = 0
+
+    for i, line := range lines {
+        line = strings.TrimSpace(line)
+
+        if found == 1 {
+            // parse line for param
+            colonIndex := strings.Index(line, ":")
+            if colonIndex > 0 {
+                key := strings.TrimSpace(line[:colonIndex])
+                value := strings.TrimSpace(line[colonIndex+1:])
+                value = strings.Trim(value, "\"") //remove quotes
+                switch key {
+                    case "title":
+                    page.Title = value
+                    case "layout":
+                    page.Layout = value
+                    case "date":
+                    page.Date, _ = time.Parse(websiteConfig.DateFormat, value)
+                    default:
+                    page.Params[key] = value
+                }
+            }
+        } else if found >= 2 {
+            // params over
+            lines = lines[i:]
+            break
+        }
+
+        if line == "---" {
+            found += 1
+        }
+    }
+
+    // convert markdown content
+    content := strings.Join(lines, "\n")
+    page.Content = render([]byte(content))
+}
--- presdo.go
+++ presdo.go
@@ -17,9 +17,6 @@ import (
     "time"
     "net/http"
     "html/template"
-//    "compress/gzip"
-
-    "github.com/russross/blackfriday"
 )
 
 const VERSION = "0.0.1"
@@ -39,6 +36,7 @@ type cacheTimes struct {
     Index      int
 }
 
+// Page struct.
 type Page struct {
   Title, Category, Layout, Url string
   Content                      template.HTML
@@ -159,13 +157,14 @@ func getRequestURIStaticFile(requestURI string) (interface{}, error) {
     }, nil
 }
 
+// Recreate cached file when markdown was changed
 func recreateCachedFileIfChanged(requestFilePath string, markdownPath string, cachedPath string) {
     markdownStat, err1 := os.Stat(markdownPath)
     cachedStat, err2   := os.Stat(cachedPath)
 
     if err1 == nil && err2 == nil {
         if cachedStat.ModTime() != markdownStat.ModTime() {
-            parseMarkdownFile(requestFilePath, markdownPath)
+            markdown.Parse(requestFilePath, markdownPath)
         }
     }
 }
@@ -175,9 +174,6 @@ func responseMarkdownFile(w http.ResponseWriter, r *http.Request) error {
     requestFilePath := paths.Request(r.RequestURI)
     cachedPath      := paths.Cache(requestFilePath)
     markdownPath    := paths.Markdown(requestFilePath)
-
-    // define headers to send to client
-    w.Header().Add("Content-Type", "text/html; charset=" + websiteConfig.Encode)
 
     // if markdown not exist
     if _, err := os.Stat(markdownPath); err != nil {
@@ -189,80 +185,16 @@ func responseMarkdownFile(w http.ResponseWriter, r *http.Request) error {
 
     // if not exist cached file, parse markdown content and create HTML file synchronous
     if cachedStat, errStat = os.Stat(cachedPath); errStat != nil {
-        parseMarkdownFile(requestFilePath, markdownPath)
+        markdown.Parse(requestFilePath, markdownPath)
+        cachedStat, _ = os.Stat(cachedPath)
     }
 
     server.Content(w, r, cachedPath, cachedStat.ModTime())
 
     go recreateCachedFileIfChanged(requestFilePath, markdownPath, cachedPath)
 
-    //if cachedStat, err := os.Stat(cachedPath); err == nil {
-    //    // send cached content to user request
-    //    if cacheFileContent, err := os.Open(cachedPath); err == nil {
-    //        server.Content(w, r, cachedPath, cachedStat.ModTime(), cacheFileContent)
-    //
-    //        // if original content was changed, parse markdown file (async)
-    //        if cachedStat.ModTime() != markdownStat.ModTime() {
-    //            go parseMarkdownFile(requestFilePath, markdownPath)
-    //        }
-    //    }
-    //} else {
-    //    pageContent = parseMarkdownFile(requestFilePath, markdownPath)
-    //    fmt.Fprintf(w, string(pageContent))
-    //}
-
     return nil
 }
 
-func parseMarkdownFile(requestFilePath string, markdownPath string) template.HTML {
-    markdownContent, _ := ioutil.ReadFile(markdownPath)
 
-    pageHtml := view.HTML(&Page{
-        Layout:   "page",
-        Title:    "Página de teste!",
-        Content:  markdownRender(markdownContent),
-        Date:     time.Now(),
-        Updated:  time.Now(),
-        Params:   make(map[string]string),
-    })
 
-    log.Println("pageHtml", pageHtml)
-
-    saveParsedMarkdownFile(requestFilePath, markdownPath, pageHtml)
-
-    return pageHtml
-}
-
-func saveParsedMarkdownFile(requestFilePath string, markdownPath string, parsedMarkdown template.HTML) {
-    cachedPath := paths.Cache(requestFilePath)
-
-    if err := ioutil.WriteFile(cachedPath, []byte(parsedMarkdown), 0644); err != nil {
-        log.Println("Error on create markdown cache file: ", err)
-    } else {
-        if markdownStat, err := os.Stat(markdownPath); err == nil {
-            os.Chtimes(cachedPath, markdownStat.ModTime(), markdownStat.ModTime())
-        }
-    }
-}
-
-// configure markdown render options
-// See blackfriday markdown source for details
-func markdownRender(content []byte) template.HTML {
-  htmlFlags := 0
-  //htmlFlags |= blackfriday.HTML_SKIP_SCRIPT
-  htmlFlags |= blackfriday.HTML_USE_XHTML
-  htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
-  htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
-  htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
-  renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
-
-  extensions := 0
-  extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
-  extensions |= blackfriday.EXTENSION_TABLES
-  extensions |= blackfriday.EXTENSION_FENCED_CODE
-  extensions |= blackfriday.EXTENSION_AUTOLINK
-  extensions |= blackfriday.EXTENSION_STRIKETHROUGH
-  extensions |= blackfriday.EXTENSION_SPACE_HEADERS
-
-  return template.HTML(blackfriday.Markdown([]byte(content), renderer, extensions))
-}
