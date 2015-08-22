2719d01efabbe74ff6e37a417c21a12e0b0f240a
Gustavo Paes <gustavo.paes@gmail.com>
2015-08-22 13:40:33 -0300

    Use w.Write instead of http.ServerContent to use WriteHeader for 404 error status code

--- http.go
+++ http.go
@@ -6,8 +6,10 @@ import (
     "mime"
     "fmt"
     "time"
+    "io/ioutil"
     "log"
     "os"
+    "strings"
 )
 
 type ServerStruct struct { }
@@ -28,28 +30,63 @@ func (s *ServerStruct) Run() {
 func (s *ServerStruct) request(w http.ResponseWriter, r *http.Request) {
     LogRequest(w, r)
 
+    pathRequest := paths.Request(r.RequestURI)
+
     // add default headers
     w.Header().Add("Server", "Presdo " + VERSION)
     w.Header().Add("Vary", "Accept-Encoding")
 
-    if fileStat, err := getRequestURIStaticFile( paths.Request(r.RequestURI) ); err == nil {
+    if fileStat, err := getRequestURIStaticFile( pathRequest ); err == nil {
         s.Static(w, r, fileStat.(FileInfo))
     } else {
-        // it is not an static file, look for markdown content
-        err := responseMarkdownFile(w, r)
+        // check extention
+        if websiteConfig.Ext != filepath.Ext(pathRequest) {
+            s.NotFound(w, r)
+        } else {
+            // it is not an static file, look for markdown content
+            err := responseMarkdownFile(w, r)
 
-        if os.IsNotExist(err) {
-            LogResponse(w, r)
-            http.NotFound(w, r)
+            if os.IsNotExist(err) {
+                s.NotFound(w, r)
+            }
         }
     }
+}
+
+func (s *ServerStruct) NotFound(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, websiteConfig.NotFound, 302)
 }
 
 // Send to client some static file
 func (s *ServerStruct) Static(w http.ResponseWriter, r *http.Request, fileStat FileInfo) {
     statiFilePath := paths.Public(fileStat.FullPath)
+    http.ServeFile(w, r, statiFilePath)
+}
 
-    s.Content(w, r, statiFilePath, fileStat.Stat.ModTime())
+var unixEpochTime = time.Unix(0, 0)
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// http://golang.org/src/net/http/fs.go#L261
+func checkLastModified(w http.ResponseWriter, r *http.Request, modtime time.Time) bool {
+    if modtime.IsZero() || modtime.Equal(unixEpochTime) {
+        // If the file doesn't have a modtime (IsZero), or the modtime
+        // is obviously garbage (Unix time == 0), then ignore modtimes
+        // and don't process the If-Modified-Since header.
+        return false
+    }
+
+    // The Date-Modified header truncates sub-second precision, so
+    // use mtime < t+1s instead of mtime <= t to check for unmodified.
+    if t, err := time.Parse(http.TimeFormat, r.Header.Get("If-Modified-Since")); err == nil && modtime.Before(t.Add(1*time.Second)) {
+        h := w.Header()
+        delete(h, "Content-Type")
+        delete(h, "Content-Length")
+        w.WriteHeader(http.StatusNotModified)
+        return true
+    }
+
+    w.Header().Set("Last-Modified", modtime.UTC().Format(http.TimeFormat))
+    return false
 }
 
 // Send content response
@@ -57,10 +94,17 @@ func (s *ServerStruct) Content(w http.ResponseWriter, r *http.Request, filePath 
     ext := filepath.Ext(filePath)
 
     // define headers to send to client
-    w.Header().Add("Content-Type", mime.TypeByExtension(ext) + "; charset=" + websiteConfig.Encode)
+    w.Header().Add("Content-Type", mime.TypeByExtension(ext))
 
-    if fileContent, err := os.Open(filePath); err == nil {
+    if fileContent, err := ioutil.ReadFile(filePath); err == nil {
+        if strings.Contains(filePath, websiteConfig.NotFound) {
+            w.WriteHeader(http.StatusNotFound)
+        }
+
+        checkLastModified(w, r, modTime)
+
+        w.Write([]byte(fileContent))
+
         LogResponse(w, r)
-        http.ServeContent(w, r, filePath, modTime, fileContent)
     }
 }
